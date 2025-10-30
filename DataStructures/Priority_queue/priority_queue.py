9c9076a4dc5f830f7c344d4a5ef6e1cd6ad5c1d0
tomasjara2 <t.jara1@uniandes.edu.co>
2025-10-30 08:43:37 -0500

    b

--- DataStructures/Priority_queue/priority_queue.py
+++ DataStructures/Priority_queue/priority_queue.py
@@ -20,12 +20,19 @@ def new_heap(is_min_pq=True):
         }
     
 def default_compare_higher_value(father_node, child_node):
-    if pqe.get father_node
+    if pqe.get_priority(father_node) >= pqe.get_priority(child_node):
+        return True
+    return False
 
 def default_compare_lower_value(father_node, child_node):
+    if pqe.get_priority(father_node) <= pqe.get_priority(child_node):
+        return True
+    return False
 
 
 
+def priority(my_heap, parent, child):
+    return my_heap["cmp_function"](parent, child)
 
 def is_empty(my_heap):
     empty=False
