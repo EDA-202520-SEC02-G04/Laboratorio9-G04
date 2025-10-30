e19fdab0c31a16de4457b2e1ad636d3044119bc7
tomasjara2 <t.jara1@uniandes.edu.co>
2025-10-30 08:39:39 -0500

    a

--- DataStructures/Priority_queue/priority_queue.py
+++ DataStructures/Priority_queue/priority_queue.py
@@ -1,2 +1,25 @@
+from DataStructures.Priority_queue import pq_entry as pqe
+
 def new_heap(is_min_pq=True):
+    if is_min_pq:
+        return {
+            'elements': {
+                 'elements': [None],
+                'size': 1
+            },
+            'size': 0,
+            'cmp_function': default_compare_lower_value()
+        }
+    return {
+            'elements': {
+                 'elements': [None],
+                'size': 1
+            },
+            'size': 0,
+            'cmp_function': default_compare_higher_value()
+        }
     
+def default_compare_higher_value(father_node, child_node):
+    if pqe.get father_node
+
+def default_compare_lower_value(father_node, child_node):
